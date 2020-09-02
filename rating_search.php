5b858b7516cd7297511945d09fb632b203a777cd
stevensc9277 <60583258+stevensc9277@users.noreply.github.com>
2020-09-03 11:38:15 +1200

    possible rating searches

--- rating_search.php
+++ rating_search.php
@@ -7,11 +7,18 @@ if(isset($_POST['find_rating']))
 {
 
 $amount = test_input(mysqli_real_escape_string($dbconnect, $_POST['amount']));
+$stars = test_input(mysqli_real_escape_string($dbconnect, $_POST['stars']));
     
+if ($amount=="exactly")
+{
     $find_sql="SELECT *
 FROM `91879_book_reviews`
 WHERE `genre` LIKE '%$genre%'
 LIMIT 0 , 30";
+    
+}
+    
+    
     $find_query=mysqli_query($dbconnect, $find_sql);
     $find_rs=mysqli_fetch_assoc($find_query);
     $count=mysqli_num_rows($find_query);
